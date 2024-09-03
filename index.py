c4083892b55023597fc6e8de84fb01f869cf7751
Amir <amir.hac@outlook.com>
2024-09-03 16:41:14 +0200

    Update index.py

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ class HelloWorld(object):
 
 
 cherrypy.config.update({'server.socket_host': '0.0.0.0',
-                        'server.socket_port': 80,
+                        'server.socket_port': 8080,
                        })
 
 cherrypy.quickstart(HelloWorld())
