b15d2d318e6fcbf2d48159ae0260a8089b68484f
Amir <amir.hac@outlook.com>
2024-09-03 16:38:04 +0200

    config

--- index.py
+++ index.py
@@ -5,4 +5,9 @@ class HelloWorld(object):
     def index(self):
         return "Welcome to CherryPy!"
 
+
+cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                        'server.socket_port': 80,
+                       })
+
 cherrypy.quickstart(HelloWorld())
